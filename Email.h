a4cb32f8e91f9199bf87930756fd2e3cc1741a01
Salem B <136395670+sqd2@users.noreply.github.com>
2024-10-30 16:03:29 +0800

    Update Email.h
    changed it to class structure instead of struct and added relevant getter methods

--- Email.h
+++ Email.h
@@ -1,14 +1,50 @@
-// Email.h
-#ifndef EMAIL_H
-#define EMAIL_H
+#ifndef EMAIL_HPP
+#define EMAIL_HPP
 
+#include <iostream>
+#include <ctime>
 using namespace std;
 
-struct Email {
+class Email {
+private:
     string sender;
+    string recipient;
     string subject;
     string content;
-    int priority; // Lower number = higher priority
+    time_t timestamp;
+    int priority;
+    bool isSpam;
+
+public:
+    Email(string snd, string rcp, string sub, string cnt, int prio):
+        sender(snd), recipient(rcp), subject(sub), content(cnt), priority(prio), isSpam(false) {
+        timestamp = time(nullptr); // current time
+    }
+
+    void markSpam(){
+        isSpam = true; 
+    }
+    bool getSpam() const {
+        return isSpam; 
+    }
+    int getPriority() const {
+        return priority; 
+    }
+    time_t getTime() const { 
+        return timestamp; 
+    }
+    string getSender() const {
+        return sender;
+    }
+    string getRecipient() const {
+        return recipient;
+    }
+    string getSubject() const {
+        return subject;
+    }
+    string getContent() const {
+        return content;
+    }
 };
-    //test
+
 #endif
